7d77e30d00d893149865e5877a8e4177ce4a66ca
hrhopp <haydenhoppe2003@gmail.com>
2025-05-01 17:00:48 -0500

    view update

--- DBProject.sql
+++ DBProject.sql
@@ -450,7 +450,7 @@ WHERE big.Victim_name = 'Abbey Dean'; -- Change name for different results
 -- views
 -- Example view for a victim
 Create view VictimAccess as 
-Select v.Victim_Name, i.Incident_ID, i.Fire_Type, i.Incident_Date, i.Zipcode, Includes.Plan_ID, Includes.Person_ID as EmployeeNum, Includes.Victim_ID, Includes.Site_ID, p.Plan_Type, p.Information as PlanInfo, r.Location as RecoverySiteLocation
+Select distinct v.Victim_Name, i.Incident_ID, i.Fire_Type, i.Incident_Date, i.Zipcode, Includes.Plan_ID, Includes.Person_ID as EmployeeNum, Includes.Victim_ID, Includes.Site_ID, p.Plan_Type, p.Information as PlanInfo, r.Location as RecoverySiteLocation
 From Incident i, Includes, Victims v, Plans p, RecoverySite r
 Where v.Victim_ID = Includes.Victim_ID 
 AND Includes.plan_id = p.plan_id
@@ -462,7 +462,7 @@ select * from VictimAccess;
 
 -- Example view for an Employee
 Create View EmployeeAccess as
-Select e.Employee_name, p.Plan_ID, p.Plan_Type, p.Information, v.Victim_Name, i.Fire_Type, i.Incident_date, i.Danger_Level, b.BP_Name, r.Resource_Type, r.Resource_Name, rec.Site_Name, rec.Location , i.Zipcode
+Select distinct e.Employee_name, p.Plan_ID, p.Plan_Type, p.Information, v.Victim_Name, i.Fire_Type, i.Incident_date, i.Danger_Level, b.BP_Name, r.Resource_Type, r.Resource_Name, rec.Site_Name, rec.Location , i.Zipcode
 From Employee e, Plans p, Includes, Incident i, WorksFor w, BusinessProcess b, Resources r, Victims v, RecoverySite rec
 Where e.Person_ID = Includes.Person_ID
 AND p.Plan_ID = Includes.Plan_ID
@@ -471,7 +471,7 @@ AND i.Incident_ID = p.Incident_ID
 AND w.Process_Id = b.Process_ID
 AND b.Resource_ID = r.Resource_ID
 AND v.Victim_ID = Includes.Victim_ID
-AND i.Zipcode = rec.Zipcode
+AND rec.Site_ID = Includes.Site_ID
 AND e.Employee_Name = 'Rachel Greene';
 
 select * from EmployeeAccess;
